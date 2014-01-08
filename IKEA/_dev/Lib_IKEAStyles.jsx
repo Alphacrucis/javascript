205b6dfc848766df0e248b3447ff500e9ae0d4f6
Javier Ramos <javier.ramoshidalgo@gmail.com>
2014-01-08 14:24:28 +0000

    IKEA Styles

---
+++ IKEA/_dev/Lib_IKEAStyles.jsx
@@ -0,0 +1,74 @@
+﻿/** IKEA Styles API
+    Author: Javier Ramos
+    
+    Dependencies: extentables
+    
+    Apply indesign styles to IKEA articles
+    */
+
+#target "indesign"
+#strict on
+
+#include "~/_scripts/frameworks/Extendables/extendables.jsx"
+#include "Lib_IKEAProducts.jsx"
+
+// extends IKEA module with a submodule for styling capabilities
+IKEA.Styles = IKEA.Styles || (function() {
+    var exported = { name:'IKEA Styles submodule' };
+    
+    exported.createDefaultStyles = function(doc) {
+        var default_styles = ['Articulo'];
+        
+        default_styles.forEach(function(style) {
+            if (!doc.paragraphsStyles.item(style).isValid) {
+                doc.paragraphsStyles.add({
+                    name:style,
+                    fontName: 'Verdana IKEA',
+                    pointSize: 7.25, 
+                    fontStyle: 'Normal', 
+                    tracking:-40, 
+                    kerningValue: 9.5
+
+           }
+       });
+    };
+
+    exported.applyStyles = function(el) {
+        var style;
+
+	exported.createDefaultStyles(  )
+
+        // apply styles
+        try {
+            style = IKEA.paragraphStyle('Articulo', IKEA.PS_ARTICULO);
+            el.paragraphs.everyItem().applyParagraphStyle(style);
+            el.paragraphs.everyItem().clearOverrides();
+        } catch(e) { $.writeln(e); }
+        
+        var applyStyles = function applyStyle(xmlE) {
+            if (xmlE.markupTag.name === 'Article'){
+                xmlE.children().forEach(applyStyle);
+            } else {
+                if (xmlE.markupTag.name === 'name'){
+                    xmlE.words.everyItem().fontStyle = 'Bold';
+                } else if (xmlE.markupTag.name === 'description'){
+                    xmlE.words.everyItem().fontStyle = 'Bold';
+                    collectionToArray(xmlE.words).forEach(function(w) {
+                        w.contents = w.contents.toString().toLowerCase();
+                    });
+                } else if (xmlE.markupTag.name === 'price'){
+                    xmlE.words.everyItem().fontStyle = 'Bold';
+                    var word = xmlE.words.everyItem().getElements().filter(function(w) { 
+                            return w.contents.indexOf(price_symbol) >= 0;
+                        });
+                } else {
+                    xmlE.words.everyItem().fontStyle = 'Normal';
+                }
+            } 
+        };
+        
+        el.children().forEach(applyStyle);
+    }
+
+    return exported;
+})());
---
+++ util/evaluate.jsx
@@ -0,0 +1,28 @@
+﻿#target "indesign"
+#targetengine "medianis"
+#strict on
+
+$.evalFile('~/_scripts/frameworks/Extendables/extendables.jsx');
+
+var ui = require("ui");
+
+var Evaluate = Evaluate || (function() {
+    if (! Evaluate.is_running) {
+            
+        function main() {
+            var dialog;
+            
+            dialog = new ui.Palette('Evaluar expresión'); //.with(mixins);
+            dialog.window.name = 'Evaluador de Expresiones';
+            dialog.column('mainFrame').input('exp_edit');
+            dialog.mainFrame.input('console');
+            
+            dialog.show();
+        }
+
+        main();
+    };
+    
+    return { is_running: true };
+})();
+
